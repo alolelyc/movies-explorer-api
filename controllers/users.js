bdf87ba437f021e9040f4683b8c83b059c5c6449
Olga Elyakova <alolelyc@yandex.ru>
2023-06-10 15:28:30 +0300

    ДРабота

--- controllers/users.js
+++ controllers/users.js
@@ -59,7 +59,7 @@ module.exports.login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'none',
-        secure: true,
+
       })
         // отправим токен пользователю
         .send({ token });
